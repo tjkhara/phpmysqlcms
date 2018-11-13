a4d2584f9b7002347c209b310b38c4b0f22389c7
Tajeshwar Khara <tkhara@gmail.com>
2018-11-13 22:58:57 +0530

    Challenge complete. Completed the external include of the navigation.

---
+++ challenge/includes/nav.php
@@ -0,0 +1,9 @@
+<nav>
+      <ol>
+        <li><a href="<?= $siteroot; ?>/index.php">HOME</a></li>
+        <li><a href="<?= $siteroot; ?>/artists.php">ARTISTS</a></li>
+        <li><a href="<?= $siteroot; ?>/schedule/index.php">SCHEDULE</a></li>
+        <li><a href="<?= $siteroot; ?>/venue.php">VENUE/TRAVEL</a></li>
+        <li><a href="<?= $siteroot; ?>/register/index.php">REGISTER</a></li>
+      </ol>
+</nav>
