71ad39c1e7528cf931151499bcfffef876ea4e03
Tajeshwar Khara <tkhara@gmail.com>
2018-11-13 22:28:01 +0530

    PHP include example.

---
+++ Include_Example/parent.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Using server-side includes</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Including External Files</h1>
+<p>This paragraph is in the original file.</p>
+<?php include './includes/para.html'; ?>
+<p>This is also in the original file.</p>
+<?php include './includes/para.html'; ?>
+</body>
+</html>
