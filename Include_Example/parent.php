b3064eb79fb91bfe9803d48c73f5fc0a994aa329
Tajeshwar Khara <tkhara@gmail.com>
2018-11-13 22:32:33 +0530

    PHP include_once include once example.

--- Include_Example/parent.php
+++ Include_Example/parent.php
@@ -8,9 +8,9 @@
 <body>
 <h1>Including External Files</h1>
 <p>This paragraph is in the original file.</p>
-<?php require './includes/para.html'; ?>
+<?php include_once './includes/para.html'; ?>
 <p>This is also in the original file.</p>
-<?php include './includes/para.html'; ?>
+<?php include_once './includes/para.html'; ?>
 <?php require './includes/copyright.php' ?>
 <p><?= lyn_copyright(2015); ?> Tajeshwar Singh Khara</p>
 </body>
