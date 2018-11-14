3909638f007ef2cae8dcf3f9567ac7b7d209cf0d
Tajeshwar Khara <tkhara@gmail.com>
2018-11-14 22:30:31 +0530

    Turning on display errors while using a remote server.

--- 08_01_Errors/errors.php
+++ 08_01_Errors/errors.php
@@ -1,20 +1,17 @@
 <?php
-    set_include_path(get_include_path() . PATH_SEPARATOR .
-                        'C:\XAMPP\htdocs\sandbox\Relative_Path\includes'
-                    );
-    require 'copyright.php';
-?>
+$siteroot = '/introducingphp/ch08/08_01';
+include './includes/copyright.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Using include_path</title>
+    <title>PHP errors</title>
     <link href="styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<h1>Using the include_path</h1>
+<h1>Errors Galore!</h1>
 <p>This paragraph is in the original file.</p>
-<?php include 'para.html'; ?>
+<?php include './includes/para.html'; ?>
 <p>This is also in the original file.</p>
 <p><?= lyn_copyright(2015) ;?> David Powers</p>
 </body>
