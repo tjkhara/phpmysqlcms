2ec705ef130cb22ff0bc34a657dbdad4eef157e2
Tajeshwar Khara <tkhara@gmail.com>
2018-11-16 00:58:41 +0530

    Encoded URL parameters using urlencode.

--- globe_bank/private/functions.php
+++ globe_bank/private/functions.php
@@ -1,9 +1,24 @@
 <?php
+
+// Creates a url by appending the root to the script path sent in
+// The root ends at public the script should be everything after that
 function url_for($script_path) {
     // add the leading '/' if not present
     if($script_path[0] != '/') {
         $script_path = "/" . $script_path;
     }
     return WWW_ROOT . $script_path;
+}
+
+// URL encode the string sent in the argument
+function u($string = "")
+{
+    return urlencode($string);
+}
+
+// Raw URL encode the string sent in the argument
+function raw_u($string = "")
+{
+    return rawurlencode($string);
 }
 ?>
--- globe_bank/public/staff/subjects/show.php
+++ globe_bank/public/staff/subjects/show.php
@@ -1,3 +1,4 @@
+<?php require_once('../../../private/initialize.php'); ?>
 <?php
 // Using default value '1'
 // if $_GET['id'] is not set
@@ -6,3 +7,5 @@ $id = $_GET['id'] ?? '1';
 echo $id;
 
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br/>
