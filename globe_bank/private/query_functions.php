760f536d859a4a159f29820bb8c5f1993a1834ae
Tajeshwar Khara <tkhara@gmail.com>
2018-11-25 17:31:06 +0530

    Staff -> subjects -> show.php finding a single record from the database and displaying it when the view link is clicked.

--- globe_bank/private/query_functions.php
+++ globe_bank/private/query_functions.php
@@ -14,6 +14,22 @@ function find_all_subjects()
     return $result;
 }
 
+// Find a single subject
+// Returns an associative array
+function find_subject_by_id($id){
+    global $db;
+
+    $sql = "SELECT * FROM subjects ";
+    $sql .= "WHERE id='" . $id . "'";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+
+    $subject = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+
+    return $subject;
+}
+
 
 // Find all pages
 function find_all_pages()
--- globe_bank/public/staff/subjects/show.php
+++ globe_bank/public/staff/subjects/show.php
@@ -2,13 +2,32 @@
 <?php $page_title = "Subjects Show Page"; ?>
 <?php include_once(SHARED_PATH . '/staff_header.php'); ?>
 
+<?php
+    $id = $_GET['id'] ?? '1';
+
+    $subject = find_subject_by_id($id);
+?>
+
+
 <div id="content">
     <a href="index.php">&laquo; Back to List</a>
     <div class="subjects show">
-    <?php
-        $id = $_GET['id'] ?? '1';
-        echo "Subject ID: " . h($id);
-    ?>
+
+        <h1>Subject: <?php echo h($subject['menu_name']); ?></h1>
+        <div class="attributes">
+            <dl>
+                <dt>Menu Name</dt>
+                <dd><?php echo h($subject['menu_name']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Position</dt>
+                <dd><?php echo h($subject['position']); ?></dd>
+            </dl>
+            <dl>
+                <dt>Visible</dt>
+                <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
+            </dl>
+        </div>
     </div>
 </div>
 
